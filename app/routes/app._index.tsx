214a407d51f3f90308f2e9a9090850e254c64a5c
Md Al Amin <mdalamin212104@gmail.com>
2025-10-07 17:01:01 +0600

    added new page

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,253 +1,226 @@
-import { useEffect } from "react";
-import type {
-  ActionFunctionArgs,
-  HeadersFunction,
-  LoaderFunctionArgs,
-} from "react-router";
-import { useFetcher } from "react-router";
-import { useAppBridge } from "@shopify/app-bridge-react";
-import { authenticate } from "../shopify.server";
-import { boundary } from "@shopify/shopify-app-react-router/server";
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
-
-  return null;
-};
-
-export const action = async ({ request }: ActionFunctionArgs) => {
-  const { admin } = await authenticate.admin(request);
-  const color = ["Red", "Orange", "Yellow", "Green"][
-    Math.floor(Math.random() * 4)
-  ];
-  const response = await admin.graphql(
-    `#graphql
-      mutation populateProduct($product: ProductCreateInput!) {
-        productCreate(product: $product) {
-          product {
-            id
-            title
-            handle
-            status
-            variants(first: 10) {
-              edges {
-                node {
-                  id
-                  price
-                  barcode
-                  createdAt
-                }
-              }
-            }
-          }
-        }
-      }`,
-    {
-      variables: {
-        product: {
-          title: `${color} Snowboard`,
-        },
-      },
-    },
-  );
-  const responseJson = await response.json();
-
-  const product = responseJson.data!.productCreate!.product!;
-  const variantId = product.variants.edges[0]!.node!.id!;
-
-  const variantResponse = await admin.graphql(
-    `#graphql
-    mutation shopifyReactRouterTemplateUpdateVariant($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
-      productVariantsBulkUpdate(productId: $productId, variants: $variants) {
-        productVariants {
-          id
-          price
-          barcode
-          createdAt
-        }
-      }
-    }`,
-    {
-      variables: {
-        productId: product.id,
-        variants: [{ id: variantId, price: "100.00" }],
-      },
-    },
-  );
-
-  const variantResponseJson = await variantResponse.json();
-
-  return {
-    product: responseJson!.data!.productCreate!.product,
-    variant:
-      variantResponseJson!.data!.productVariantsBulkUpdate!.productVariants,
-  };
-};
-
 export default function Index() {
-  const fetcher = useFetcher<typeof action>();
-
-  const shopify = useAppBridge();
-  const isLoading =
-    ["loading", "submitting"].includes(fetcher.state) &&
-    fetcher.formMethod === "POST";
-  const productId = fetcher.data?.product?.id.replace(
-    "gid://shopify/Product/",
-    "",
-  );
-
-  useEffect(() => {
-    if (productId) {
-      shopify.toast.show("Product created");
-    }
-  }, [productId, shopify]);
-  const generateProduct = () => fetcher.submit({}, { method: "POST" });
-
   return (
     <s-page heading="React Router app template">
-      <s-button slot="primary-action" onClick={generateProduct}>
-        Generate a product
-      </s-button>
-
-      <s-section heading="Congrats on creating a new Shopify app 🎉">
-        <s-paragraph>
-          This embedded app template uses{" "}
-          <s-link
-            href="https://shopify.dev/docs/apps/tools/app-bridge"
-            target="_blank"
-          >
-            App Bridge
-          </s-link>{" "}
-          interface examples like an{" "}
-          <s-link href="/app/additional">additional page in the app nav</s-link>
-          , as well as an{" "}
-          <s-link
-            href="https://shopify.dev/docs/api/admin-graphql"
-            target="_blank"
-          >
-            Admin GraphQL
-          </s-link>{" "}
-          mutation demo, to provide a starting point for app development.
-        </s-paragraph>
+      <s-section>
+        <s-grid gap="base">
+          <s-grid gap="small-200">
+            <s-grid gridTemplateColumns="1fr auto auto" gap="small-300" alignItems="center">
+              <s-heading>Setup Guide</s-heading>
+              <s-button
+                accessibilityLabel="Dismiss Guide"
+                variant="tertiary"
+                tone="neutral"
+                icon="x"
+              ></s-button>
+              <s-button
+                accessibilityLabel="Toggle setup guide"
+                variant="tertiary"
+                tone="neutral"
+                icon="chevron-up"
+              ></s-button>
+            </s-grid>
+            <s-paragraph>
+              Use this personalized guide to get your store ready for sales.
+            </s-paragraph>
+            <s-paragraph tone="subdued">0 out of 3 steps completed</s-paragraph>
+          </s-grid>
+          <s-box borderRadius="base" border="base" background="base">
+            <s-box>
+              <s-grid gridTemplateColumns="1fr auto" gap="base" padding="small">
+                <s-checkbox
+                  label="Upload an image for your puzzle"
+                ></s-checkbox>
+                <s-button
+                  accessibilityLabel="Toggle step 1 details"
+                  variant="tertiary"
+                  icon="chevron-up"
+                ></s-button>
+              </s-grid>
+              <s-box padding="small" paddingBlockStart="none">
+                <s-box padding="base" background="subdued" borderRadius="base">
+                  <s-grid gridTemplateColumns="1fr auto" gap="base" alignItems="center">
+                    <s-grid gap="small-200">
+                      <s-paragraph>
+                        Start by uploading a high-quality image that will be used to create your
+                        puzzle. For best results, use images that are at least 1200x1200 pixels.
+                      </s-paragraph>
+                      <s-stack direction="inline" gap="small-200">
+                        <s-button variant="primary">
+                          Upload image
+                        </s-button>
+                        <s-button variant="tertiary" tone="neutral"> Image requirements </s-button>
+                      </s-stack>
+                    </s-grid>
+                    <s-box maxBlockSize="80px" maxInlineSize="80px">
+                      <s-image
+                        src="https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg"
+                        alt="Customize checkout illustration"
+                      ></s-image>
+                    </s-box>
+                  </s-grid>
+                </s-box>
+              </s-box>
+            </s-box>
+            <s-divider></s-divider>
+            <s-box>
+              <s-grid gridTemplateColumns="1fr auto" gap="base" padding="small">
+                <s-checkbox
+                  label="Choose a puzzle template"
+                ></s-checkbox>
+                <s-button
+                  accessibilityLabel="Toggle step 2 details"
+                  variant="tertiary"
+                  icon="chevron-down"
+                ></s-button>
+              </s-grid>
+              <s-box padding="small" paddingBlockStart="none"></s-box>
+            </s-box>
+            <s-divider></s-divider>
+            <s-box>
+              <s-grid gridTemplateColumns="1fr auto" gap="base" padding="small">
+                <s-checkbox
+                  label="Customize puzzle piece shapes"
+                ></s-checkbox>
+                <s-button
+                  accessibilityLabel="Toggle step 3 details"
+                  variant="tertiary"
+                  icon="chevron-down"
+                ></s-button>
+              </s-grid>
+              <s-box padding="small" paddingBlockStart="none"></s-box>
+            </s-box>
+          </s-box>
+        </s-grid>
       </s-section>
-      <s-section heading="Get started with products">
-        <s-paragraph>
-          Generate a product with GraphQL and get the JSON output for that
-          product. Learn more about the{" "}
-          <s-link
-            href="https://shopify.dev/docs/api/admin-graphql/latest/mutations/productCreate"
-            target="_blank"
-          >
-            productCreate
-          </s-link>{" "}
-          mutation in our API references.
-        </s-paragraph>
-        <s-stack direction="inline" gap="base">
-          <s-button
-            onClick={generateProduct}
-            {...(isLoading ? { loading: true } : {})}
-          >
-            Generate a product
-          </s-button>
-          {fetcher.data?.product && (
+
+      <s-section padding="none" accessibilityLabel="Puzzles table section">
+        <s-table>
+          <s-grid slot="filters" gap="small-200" gridTemplateColumns="1fr auto">
+            <s-text-field
+              icon="search"
+              placeholder="Searching all puzzles"
+            ></s-text-field>
             <s-button
-              href={`shopify:admin/products/${productId}`}
-              target="_blank"
-              variant="tertiary"
-            >
-              View product
-            </s-button>
-          )}
-        </s-stack>
-        {fetcher.data?.product && (
-          <s-section heading="productCreate mutation">
-            <s-stack direction="block" gap="base">
-              <s-box
-                padding="base"
-                borderWidth="base"
-                borderRadius="base"
-                background="subdued"
-              >
-                <pre style={{ margin: 0 }}>
-                  <code>{JSON.stringify(fetcher.data.product, null, 2)}</code>
-                </pre>
-              </s-box>
-
-              <s-heading>productVariantsBulkUpdate mutation</s-heading>
-              <s-box
-                padding="base"
-                borderWidth="base"
-                borderRadius="base"
-                background="subdued"
-              >
-                <pre style={{ margin: 0 }}>
-                  <code>{JSON.stringify(fetcher.data.variant, null, 2)}</code>
-                </pre>
-              </s-box>
-            </s-stack>
-          </s-section>
-        )}
-      </s-section>
-
-      <s-section slot="aside" heading="App template specs">
-        <s-paragraph>
-          <s-text>Framework: </s-text>
-          <s-link href="https://reactrouter.com/" target="_blank">
-            React Router
-          </s-link>
-        </s-paragraph>
-        <s-paragraph>
-          <s-text>Interface: </s-text>
-          <s-link
-            href="https://shopify.dev/docs/api/app-home/using-polaris-components"
-            target="_blank"
-          >
-            Polaris web components
-          </s-link>
-        </s-paragraph>
-        <s-paragraph>
-          <s-text>API: </s-text>
-          <s-link
-            href="https://shopify.dev/docs/api/admin-graphql"
-            target="_blank"
-          >
-            GraphQL
-          </s-link>
-        </s-paragraph>
-        <s-paragraph>
-          <s-text>Database: </s-text>
-          <s-link href="https://www.prisma.io/" target="_blank">
-            Prisma
-          </s-link>
-        </s-paragraph>
-      </s-section>
-
-      <s-section slot="aside" heading="Next steps">
-        <s-unordered-list>
-          <s-list-item>
-            Build an{" "}
-            <s-link
-              href="https://shopify.dev/docs/apps/getting-started/build-app-example"
-              target="_blank"
-            >
-              example app
-            </s-link>
-          </s-list-item>
-          <s-list-item>
-            Explore Shopify&apos;s API with{" "}
-            <s-link
-              href="https://shopify.dev/docs/apps/tools/graphiql-admin-api"
-              target="_blank"
-            >
-              GraphiQL
-            </s-link>
-          </s-list-item>
-        </s-unordered-list>
+              icon="sort"
+              variant="secondary"
+              interestFor="sort-tooltip"
+              commandFor="sort-actions"
+            ></s-button>
+            <s-tooltip id="sort-tooltip">
+              <s-text>Sort</s-text>
+            </s-tooltip>
+            <s-popover id="sort-actions">
+              <s-stack gap="none">
+                <s-box padding="small">
+                  <s-choice-list label="Sort by" name="Sort by">
+                    <s-choice value="puzzle-name" selected>Puzzle name</s-choice>
+                    <s-choice value="pieces">Pieces</s-choice>
+                    <s-choice value="created">Created</s-choice>
+                    <s-choice value="status">Status</s-choice>
+                  </s-choice-list>
+                </s-box>
+                <s-divider></s-divider>
+                <s-box padding="small">
+                  <s-choice-list label="Order by" name="Order by">
+                    <s-choice value="product-title" selected>A-Z</s-choice>
+                    <s-choice value="created">Z-A</s-choice>
+                  </s-choice-list>
+                </s-box>
+              </s-stack>
+            </s-popover>
+          </s-grid>
+          <s-table-header-row>
+            <s-table-header listSlot="primary">Puzzle</s-table-header>
+            <s-table-header format="numeric">Pieces</s-table-header>
+            <s-table-header>Created</s-table-header>
+            <s-table-header listSlot="secondary">Status</s-table-header>
+          </s-table-header-row>
+          <s-table-body>
+            <s-table-row clickDelegate="mountain-view-checkbox">
+              <s-table-cell>
+                <s-stack direction="inline" gap="small" alignItems="center">
+                  <s-checkbox id="mountain-view-checkbox"></s-checkbox>
+                  <s-clickable
+                    href=""
+                    accessibilityLabel="Mountain View puzzle thumbnail"
+                    border="base"
+                    borderRadius="base"
+                    overflow="hidden"
+                    inlineSize="40px"
+                    blockSize="40px"
+                  >
+                    <s-image
+                      objectFit="cover"
+                      src="https://picsum.photos/id/29/80/80"
+                    ></s-image>
+                  </s-clickable>
+                  <s-link href="">Product title</s-link>
+                </s-stack>
+              </s-table-cell>
+              <s-table-cell>16</s-table-cell>
+              <s-table-cell>Today</s-table-cell>
+              <s-table-cell>
+                <s-badge color="base" tone="success">Active</s-badge>
+              </s-table-cell>
+            </s-table-row>
+            <s-table-row clickDelegate="ocean-sunset-checkbox">
+              <s-table-cell>
+                <s-stack direction="inline" gap="small" alignItems="center">
+                  <s-checkbox id="ocean-sunset-checkbox"></s-checkbox>
+                  <s-clickable
+                    href=""
+                    accessibilityLabel="Ocean Sunset puzzle thumbnail"
+                    border="base"
+                    borderRadius="base"
+                    overflow="hidden"
+                    inlineSize="40px"
+                    blockSize="40px"
+                  >
+                    <s-image
+                      objectFit="cover"
+                      src="https://picsum.photos/id/12/80/80"
+                    ></s-image>
+                  </s-clickable>
+                  <s-link href="">Ocean Sunset</s-link>
+                </s-stack>
+              </s-table-cell>
+              <s-table-cell>9</s-table-cell>
+              <s-table-cell>Yesterday</s-table-cell>
+              <s-table-cell>
+                <s-badge color="base" tone="success">Active</s-badge>
+              </s-table-cell>
+            </s-table-row>
+            <s-table-row clickDelegate="forest-animals-checkbox">
+              <s-table-cell>
+                <s-stack direction="inline" gap="small" alignItems="center">
+                  <s-checkbox id="forest-animals-checkbox"></s-checkbox>
+                  <s-clickable
+                    href=""
+                    accessibilityLabel="Forest Animals puzzle thumbnail"
+                    border="base"
+                    borderRadius="base"
+                    overflow="hidden"
+                    inlineSize="40px"
+                    blockSize="40px"
+                  >
+                    <s-image
+                      objectFit="cover"
+                      src="https://picsum.photos/id/324/80/80"
+                    ></s-image>
+                  </s-clickable>
+                  <s-link href="">Forest Animals</s-link>
+                </s-stack>
+              </s-table-cell>
+              <s-table-cell>25</s-table-cell>
+              <s-table-cell>Last week</s-table-cell>
+              <s-table-cell>
+                <s-badge color="base" tone="neutral">Draft</s-badge>
+              </s-table-cell>
+            </s-table-row>
+          </s-table-body>
+        </s-table>
       </s-section>
     </s-page>
   );
 }
 
-export const headers: HeadersFunction = (headersArgs) => {
-  return boundary.headers(headersArgs);
-};
--- app/routes/app.crud.tsx
+++ app/routes/app.crud.tsx
@@ -3,26 +3,223 @@ export default function CrudOpration() {
         <s-page heading="A page with CRUD operations">
             <s-section padding="base" heading="Details" >
                 <div>
-  <s-stack direction="inline" gap="base">
-    <s-text>Check our latest offers</s-text>
-    <s-link commandFor="modal" command="--show"> Fill out the survey </s-link>
-  </s-stack>
-  <s-modal id="modal" heading="Tell us about your shopping experience">
-    <s-stack gap="base">
-      <s-text>We'd love to hear about your shopping experience</s-text>
-      <s-text-area
-        rows="4"
-        label="How can we make your shopping experience better?"
-      ></s-text-area>
-      <s-button>Submit</s-button>
-    </s-stack>
-  </s-modal>
-</div>
-<s-divider></s-divider>
+                    <s-stack direction="inline" gap="base">
+                        <s-text>Check our latest offers</s-text>
+                        <s-link commandFor="modal" command="--show"> Fill out the survey </s-link>
+                    </s-stack>
+                    <s-modal id="modal" heading="Tell us about your shopping experience">
+                        <s-stack gap="base">
+                            <s-text>We`d love to hear about your shopping experience</s-text>
+                            <s-text-area
+                                rows={4}
+                                label="How can we make your shopping experience better?"
+                            ></s-text-area>
+                            <s-button>Submit</s-button>
+                        </s-stack>
+                    </s-modal>
+                </div>
+                <s-divider></s-divider>
 
-<progress value={0.5} max={1}></progress>
+                <progress value={0.8} max={1}></progress>
+                <s-date-picker defaultView="2025-10" defaultValue="2025-10-03"></s-date-picker>
+
 
             </s-section>
+
+
+            <s-section>
+  <s-grid gridTemplateColumns="1fr auto" gap="small-400" alignItems="start">
+    <s-grid
+      gridTemplateColumns="@container (inline-size <= 480px) 1fr, auto auto"
+      gap="base"
+      alignItems="center"
+    >
+      <s-grid gap="small-200">
+        <s-heading>Ready to create your custom puzzle?</s-heading>
+        <s-paragraph>
+          Start by uploading an image to your gallery or choose from one of our templates.
+        </s-paragraph>
+        <s-stack direction="inline" gap="small-200">
+          <s-button> Upload image </s-button>
+          <s-button tone="neutral" variant="tertiary"> Browse templates </s-button>
+        </s-stack>
+      </s-grid>
+      <s-stack alignItems="center">
+        <s-box maxInlineSize="200px" borderRadius="base" overflow="hidden">
+          <s-image
+            src="https://cdn.shopify.com/static/images/polaris/patterns/callout.png"
+            alt="Customize checkout illustration"
+            aspectRatio="1/0.5"
+          ></s-image>
+        </s-box>
+      </s-stack>
+    </s-grid>
+    <s-button
+      icon="x"
+      tone="neutral"
+      variant="tertiary"
+      accessibilityLabel="Dismiss card"
+    ></s-button>
+  </s-grid>
+</s-section>
+
+
+<s-section accessibilityLabel="Empty state section">
+  <s-grid gap="base" justifyItems="center" paddingBlock="large-400">
+    <s-box maxInlineSize="200px" maxBlockSize="200px">
+      <s-image
+        aspectRatio="1/0.5"
+        src="https://cdn.shopify.com/static/images/polaris/patterns/callout.png"
+        alt="A stylized graphic of four characters, each holding a puzzle piece"
+      />
+    </s-box>
+    <s-grid
+      justifyItems="center"
+      maxInlineSize="450px"
+      gap="base"
+    >
+    <s-stack alignItems="center">
+      <s-heading>Start creating puzzles</s-heading>
+      <s-paragraph>
+        Create and manage your collection of puzzles for players to enjoy.
+      </s-paragraph>
+    </s-stack>
+    <s-button-group>
+      <s-button slot="secondary-actions" aria-label="Learn more about creating puzzles"> Learn more </s-button>
+      <s-button slot="primary-action" aria-label="Add a new puzzle"> Create puzzle </s-button>
+    </s-button-group>
+    </s-grid>
+  </s-grid>
+</s-section>
+
+<s-section padding="none" accessibilityLabel="Puzzles table section">
+  <s-table>
+    <s-grid slot="filters" gap="small-200" gridTemplateColumns="1fr auto">
+      <s-text-field
+        icon="search"
+        placeholder="Searching all puzzles"
+      ></s-text-field>
+      <s-button
+        icon="sort"
+        variant="secondary"
+        interestFor="sort-tooltip"
+        commandFor="sort-actions"
+      ></s-button>
+      <s-tooltip id="sort-tooltip">
+        <s-text>Sort</s-text>
+      </s-tooltip>
+      <s-popover id="sort-actions">
+        <s-stack gap="none">
+          <s-box padding="small">
+            <s-choice-list label="Sort by" name="Sort by">
+              <s-choice value="puzzle-name" selected>Puzzle name</s-choice>
+              <s-choice value="pieces">Pieces</s-choice>
+              <s-choice value="created">Created</s-choice>
+              <s-choice value="status">Status</s-choice>
+            </s-choice-list>
+          </s-box>
+          <s-divider></s-divider>
+          <s-box padding="small">
+            <s-choice-list label="Order by" name="Order by">
+              <s-choice value="product-title" selected>A-Z</s-choice>
+              <s-choice value="created">Z-A</s-choice>
+            </s-choice-list>
+          </s-box>
+        </s-stack>
+      </s-popover>
+    </s-grid>
+    <s-table-header-row>
+      <s-table-header listSlot="primary">Puzzle</s-table-header>
+      <s-table-header format="numeric">Pieces</s-table-header>
+      <s-table-header>Created</s-table-header>
+      <s-table-header listSlot="secondary">Status</s-table-header>
+    </s-table-header-row>
+    <s-table-body>
+      <s-table-row clickDelegate="mountain-view-checkbox">
+        <s-table-cell>
+          <s-stack direction="inline" gap="small" alignItems="center">
+            <s-checkbox id="mountain-view-checkbox"></s-checkbox>
+            <s-clickable
+              href=""
+              accessibilityLabel="Mountain View puzzle thumbnail"
+              border="base"
+              borderRadius="base"
+              overflow="hidden"
+              inlineSize="40px"
+              blockSize="40px"
+            >
+              <s-image
+                objectFit="cover"
+                src="https://picsum.photos/id/29/80/80"
+              ></s-image>
+            </s-clickable>
+            <s-link href="">Mountain View</s-link>
+          </s-stack>
+        </s-table-cell>
+        <s-table-cell>16</s-table-cell>
+        <s-table-cell>Today</s-table-cell>
+        <s-table-cell>
+          <s-badge color="base" tone="success">Active</s-badge>
+        </s-table-cell>
+      </s-table-row>
+      <s-table-row clickDelegate="ocean-sunset-checkbox">
+        <s-table-cell>
+          <s-stack direction="inline" gap="small" alignItems="center">
+            <s-checkbox id="ocean-sunset-checkbox"></s-checkbox>
+            <s-clickable
+              href=""
+              accessibilityLabel="Ocean Sunset puzzle thumbnail"
+              border="base"
+              borderRadius="base"
+              overflow="hidden"
+              inlineSize="40px"
+              blockSize="40px"
+            >
+              <s-image
+                objectFit="cover"
+                src="https://picsum.photos/id/12/80/80"
+              ></s-image>
+            </s-clickable>
+            <s-link href="">Ocean Sunset</s-link>
+          </s-stack>
+        </s-table-cell>
+        <s-table-cell>9</s-table-cell>
+        <s-table-cell>Yesterday</s-table-cell>
+        <s-table-cell>
+          <s-badge color="base" tone="success">Active</s-badge>
+        </s-table-cell>
+      </s-table-row>
+      <s-table-row clickDelegate="forest-animals-checkbox">
+        <s-table-cell>
+          <s-stack direction="inline" gap="small" alignItems="center">
+            <s-checkbox id="forest-animals-checkbox"></s-checkbox>
+            <s-clickable
+              href=""
+              accessibilityLabel="Forest Animals puzzle thumbnail"
+              border="base"
+              borderRadius="base"
+              overflow="hidden"
+              inlineSize="40px"
+              blockSize="40px"
+            >
+              <s-image
+                objectFit="cover"
+                src="https://picsum.photos/id/324/80/80"
+              ></s-image>
+            </s-clickable>
+            <s-link href="">Forest Animals</s-link>
+          </s-stack>
+        </s-table-cell>
+        <s-table-cell>25</s-table-cell>
+        <s-table-cell>Last week</s-table-cell>
+        <s-table-cell>
+          <s-badge color="base" tone="neutral">Draft</s-badge>
+        </s-table-cell>
+      </s-table-row>
+    </s-table-body>
+  </s-table>
+</s-section>
+
         </s-page>
     )
 }
